6aa13b5250789868564c5582e6c1ad7d2e88ea08
Mokytojas <Mokytojas@KITM.local>
2022-02-01 13:49:55 +0200

    connected to mongo db

---
+++ controllers/trasaction-controller.js
@@ -0,0 +1,21 @@
+//@desc get all transactions
+// @route GET /api/v1/transactions
+//@access Public
+
+exports.getTransactions = (req, res, next) => {
+    res.send('GET transactions');
+}
+//@desc Add  transaction
+// @route POST /api/v1/transactions
+//@access Public
+
+exports.addTransactions = (req, res, next) => {
+    res.send('POST transactions');
+}
+//@desc Delete transaction
+// @route DELETE /api/v1/transactions/:id
+//@access Public
+
+exports.deleteTransaction = (req, res, next) => {
+    res.send('DELETE transaction');
+}
