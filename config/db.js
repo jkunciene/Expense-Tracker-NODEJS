d6b3031a25dbf31f05fedbd54f19fb1d7b4d48b5
j.kunciene <j.kunciene@gmail.com>
2022-02-01 17:55:57 +0200

    db failas

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async() => {
+    try {
+        const conn  = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,           
+            useUnifiedTopology: true
+        });
+        console.log(`Mongo DB connected: ${conn.connection.host}`.cyan.underline.bold)
+    } catch (err) {
+        console.log(`Error ${err.message}`.red);
+        process.exit(1);
+    }
+}
+module.exports = connectDB;
